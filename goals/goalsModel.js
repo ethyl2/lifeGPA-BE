ec95a6ffadf397d23794426430e4da8d9cb9609f
Heather Nuffer <zzzuzz@gmail.com>
2020-06-25 21:54:49 -0600

    Adds endpoint to get all of a user's goals, grouped by category

--- goals/goalsModel.js
+++ goals/goalsModel.js
@@ -6,6 +6,8 @@ module.exports = {
   //findBy,
   connectGoalToUser,
   getUsersGoals,
+  getUsersGoalsByCategory,
+  getUsersCategories,
   disconnectGoalToUser,
   addSuccess,
   getConnection,
@@ -18,7 +20,7 @@ module.exports = {
   updateCategory,
   removeCategory,
 };
-
+const knex = require('knex');
 const db = require('../data/db-config.js');
 
 function getGoals() {
@@ -30,7 +32,8 @@ function getGoals() {
       'goals.description',
       'goals.category_id',
       'categories.title AS category_title'
-    );
+    )
+    .orderBy('goals.category_id');
 }
 
 function addGoal(newGoal) {
@@ -69,6 +72,38 @@ function getUsersGoals(user_id) {
       'goals.description',
       'categories.title AS category_title'
     );
+}
+
+function getUsersGoalsByCategory(user_id, category_id) {
+  return db('connections')
+    .where({ user_id: user_id })
+    .join('goals', 'goals.id', 'connections.goal_id')
+    .join('categories', 'goals.category_id', '=', 'categories.id')
+    .where({ category_id: category_id })
+    .select(
+      //knex.raw('STRING_AGG(goals.title, "," AS goal_titles'),
+      'connections.id AS connection_id',
+      'goals.id AS goal_id',
+      'goals.category_id AS category_id',
+      'goals.title',
+      'goals.description',
+      'categories.title AS category_title'
+    )
+    .orderBy('goals.category_id');
+}
+
+function getUsersCategories(user_id) {
+  return db('connections')
+    .where({ user_id: user_id })
+    .join('goals', 'goals.id', 'connections.goal_id')
+    .join('categories', 'goals.category_id', '=', 'categories.id')
+    .select(
+      'categories.title AS category_title',
+      'categories.id AS category_id',
+      'categories.description AS category_description'
+    )
+    .distinct()
+    .orderBy('categories.id');
 }
 
 function disconnectGoalToUser(user_id, goal_id) {
